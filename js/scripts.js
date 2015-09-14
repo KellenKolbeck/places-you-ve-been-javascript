50e2e17b12ccc47fedaa5c1e7794505d3e3c3666
Janice Laset-Parkerson & Kellen Kolbeck <student@epicodus.com>
2015-09-14 14:31:59 -0700

    Diplay a place with its characteristics.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,28 @@ $(document).ready(function() {
   $("form#new-place").submit(function(event) {
     event.preventDefault();
 
-    var inputtedPlace = $("input#place").val();
-    var inputtedLandmark = $("input#landmark").val();
-    var inputtedTimeOfYear = $("input#time-of-year").val();
-    var inputtedNote = $("input#note").val();
+    var inputtedPlace = $("input.place-name").val();
+    var inputtedLandmark = $("input.landmark").val();
+    var inputtedTimeOfYear = $("input.time-of-year").val();
+    var inputtedNote = $("input.note").val();
 
     var newPlace = {place: inputtedPlace, landmark: inputtedLandmark, timeOfYear: inputtedTimeOfYear, note: inputtedNote};
 
     $("ul#places-you-been").append("<li><span class='place'>" + newPlace.place + "</span></li>");
 
-    $("input#place").val("");
-    $("input#landmark").val("");
-    $("input#time-of-year").val("");
-    $("input#note").val("");
+  $(".place").last().click(function() {
+    $("#place-info").show();
+    $("#place-info h2").text(newPlace.place);
+    $(".place-name").text(newPlace.place);
+    $(".landmark").text(newPlace.landmark);
+    $(".time-of-year").text(newPlace.timeOfYear);
+    $(".note").text(newPlace.note);
+  });
+
+    $("#show-places").show();
+    $("input.place-name").val("");
+    $("input.landmark").val("");
+    $("input.time-of-year").val("");
+    $("input.note").val("");
   });
 });
